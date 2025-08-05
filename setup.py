8318f96c897982018e9bc98dd04dfcbefd52ea40
Juhil Kikani <juhilkikani07@gmail.com>
2025-08-05 12:31:36 +0530

    Setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+      ''' The function returns the list of requirements'''
+      requirements=[]
+      with open(file_path) as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n"," ") for req in requirements]
+            if HYPEN_E_DOT in requirements:
+                  requirements.remove(HYPEN_E_DOT)
+      return requirements
+
+setup(
+name='ML Project',
+version='0.1',
+description='This is end to end ML Project with deployment',
+author='Juhil Kikani',
+author_email='juhilkikani07@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt'),
+)
